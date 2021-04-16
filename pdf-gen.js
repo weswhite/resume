1f7d7d8ab2d7b1764660f0c3f03bf8a517735097
Wes White <wtealwhite@gmail.com>
2021-04-16 15:20:37 -0600

    fixed pdf gen

--- pdf-gen.js
+++ pdf-gen.js
@@ -1,9 +1,22 @@
- var fs = require('fs')
- var html = fs.readFileSync('./resume.html', 'utf8')
+var fs = require("fs");
+var html = fs.readFileSync("./resume.html", "utf8");
 
-var conversion = require("phantom-html-to-pdf")();
-conversion({ html: html }, function(err, pdf) {
-  var output = fs.createWriteStream('./wes-white-resume-10-10.pdf')
-  console.log(pdf.numberOfPages);
-  pdf.stream.pipe(output);
+// var conversion = require("phantom-html-to-pdf")();
+// conversion({ html: html }, function(err, pdf) {
+//   var output = fs.createWriteStream('./wes-white-resume-10-10.pdf')
+//   console.log(pdf.numberOfPages);
+//   pdf.stream.pipe(output);
+// });
+
+var html_to_pdf = require("html-pdf-node");
+
+let options = { format: "A4", margin: { right: 50, left: 50 } };
+// Example of options with args //
+// let options = { format: 'A4', args: ['--no-sandbox', '--disable-setuid-sandbox'] };
+const file = { content: html };
+html_to_pdf.generatePdf(file, options).then((pdfBuffer) => {
+  console.log("PDF Buffer:-", pdfBuffer);
+  // const output = fs.createWriteStream("wes-white-resume-4-21.pdf");
+  // pdfBuffer.pipe(output);
+  fs.writeFileSync("wes-white-resume-4-21.pdf", pdfBuffer);
 });
